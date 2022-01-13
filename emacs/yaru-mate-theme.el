7da98606e9f815a41dab780cdbc6a4cf8332b0fa
Joshua Peisach <itzswirlz2020@outlook.com>
2022-01-13 14:02:56 -0500

    emacs: Add MATE themes

---
+++ emacs/yaru-mate-theme.el
@@ -0,0 +1,105 @@
+;;; yaru-mate-theme.el ---
+
+(deftheme yaru-mate
+  "Ubuntu community theme. Better than a burrito. (MATE variant)")
+
+(let ((class '((class color) (min-colors 89)))
+      ;; Primary Accent
+      (primary "#87A851")
+
+      ;; Blacks
+      (black "#000000")
+      (jet "#181818")
+      (inkstone "#333333")
+      (slate "#5D5D5D")
+      (graphite "#666666")
+
+      ;; Whites
+      (white "#FFFFFF")
+      (porcelain "#F7F7F7")
+      (chalk "#E5E5E5")
+      (silk "#CDCDCD")
+      (ash "#878787")
+
+      ;; Purples
+      (aubergine "#924D8B")
+      (purple "#762572")
+      (light_aubergine "#77216F")
+      (mid_aubergine "#5E2750")
+      (dark_aubergine "#2C001E")
+
+      ;; Reds
+      (red "#c7162b")
+
+      ;; Oranges
+      (orange "#E95420")
+      (satsuma "#ED764D")
+      (clementine "#F29879")
+
+      ;; Yellows
+      (yellow "#f99b11")
+
+      ;; Greens
+      (green "#0e8420")
+
+      ;; Blues
+      (blue "#19B6EE")
+      (linkblue "#007aa6")
+      (darkblue "#335280"))
+
+  (custom-theme-set-faces
+   'yaru-mate
+   ;; UI
+   `(default ((,class (:foreground ,jet :background ,white))))
+   `(highlight ((,class (:foreground ,white :background ,primary))))
+   `(region ((,class (:foreground ,white :background ,primary))))
+   `(cursor ((,class (:foreground ,orange))))
+   `(line-number-current-line ((,class (:background ,silk))))
+   `(line-number ((,class (:foreground ,jet :background ,silk :weight bold))))
+   `(match ((,class (:foreground ,white :background ,green))))
+   `(show-paren-match ((,class (:foreground ,green :background ,porcelain))))
+   `(show-paren-match-expression ((,class (:foreground ,green :background ,porcelain))))
+   `(show-paren-mismatch ((,class (:foreground ,red :background ,porcelain))))
+   `(trailing-whitespace ((,class (:foreground ,graphite :background ,porcelain))))
+   `(isearch ((,class (:foreground ,white :background ,green))))
+   `(isearch-fail ((,class (:foreground ,white :background ,red))))
+   ;; Emacs Specific
+   `(border ((,class (:background ,silk))))
+   `(internal-border ((,class (:background ,silk))))
+   `(minibuffer-prompt ((,class (:foreground ,primary))))
+   `(mode-line ((,class (:background ,black))))
+   `(mode-line-inactive ((,class (:background ,jet))))
+
+   ;; Comments
+   `(custom-comment ((,class (:foreground ,aubergine :slant italic))))
+   `(font-lock-comment-face ((,class (:foreground ,aubergine :slant italic))))
+   `(font-lock-comment-delimiter-face ((,class (:foreground ,aubergine :slant italic))))
+   `(custom-documentation ((,class (:foreground ,aubergine :slant italic))))
+   `(font-lock-doc-face ((,class (:foreground ,aubergine :slant italic))))
+   `(link ((,class (:foreground ,black :underline t))))
+   `(custom-link ((,class (:foreground ,black :underline t))))
+
+   ;; Constants and Variables
+   `(font-lock-constant-face ((,class (:foreground ,green))))
+   `(font-lock-string-face ((,class (:foreground ,yellow))))
+   `(font-lock-keyword-face ((,class (:foreground ,green))))
+   `(font-lock-builtin-face ((,class (:foreground ,green :weight bold))))
+   `(font-lock-variable-name-face ((,class (:foreground ,black))))
+   `(font-lock-preprocessor-face ((,class (:foreground ,black :weight bold))))
+   `(variable-pitch ((,class (:foreground ,black :weight bold))))
+
+   ;; Identifiers
+   `(font-lock-function-name-face ((,class (:foreground ,light_aubergine :weight bold))))
+
+   ;; Types
+   `(font-lock-type-face ((,class (:foreground ,linkblue :weight bold))))
+
+   ;; Annotations
+   `(error ((,class (:foreground ,red :background ,porcelain :weight bold :underline t))))
+   `(warning ((,class (:foreground ,orange :background ,porcelain :weight bold :underline t))))
+   `(font-lock-warning-face ((,class (:foreground ,orange :background ,porcelain :weight bold :underline t))))
+   `(underline ((,class (:foreground ,jet :underline t))))))
+
+(provide-theme 'yaru-mate)
+
+;;; yaru-mate-theme.el ends here
